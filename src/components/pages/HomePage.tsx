771dc4cdc1489561f8a4673e6b34beb1a08c5d58
Elvis Carrasco <elvissamir18@gmail.com>
2023-03-08 04:09:04 -0400

    Change project structure

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -2,14 +2,12 @@ import { Button, Flex, Heading, Link } from "@chakra-ui/react"
 import { Link as RouterLink } from "react-router-dom"
 import routes from "../../core/routes"
 import ToggleModeBtn from "../common/ToggleModeBtn"
-import { MagicButton } from 'te-testdef'
 
 const HomePage = () => {
     return (    
         <Flex flexDirection='column'>
             <Heading>Home Page</Heading>
             <Link as={RouterLink} to={routes.customers.url}>{routes.customers.name}</Link>
-            <MagicButton name="Super great!" />
             <Button size='md' variant='outline'>Hola</Button>
             <ToggleModeBtn />
         </Flex>
